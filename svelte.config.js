88e32a8ca0634e989b33178b0416009c980a485d
Justin Bennett <git@just-be.dev>
2025-11-15 22:21:34 -0500

    Update svelte config; setup webtui styles

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,38 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.svx', '.md'],
+			layout: {
+				blog: join(__dirname, './src/lib/layouts/blog.svelte'),
+				project: join(__dirname, './src/lib/layouts/project.svelte'),
+				research: join(__dirname, './src/lib/layouts/research.svelte'),
+				_: join(__dirname, './src/lib/layouts/default.svelte'),
+			},
+			smartypants: {
+				dashes: 'oldschool',
+			},
+		}),
+	],
+	kit: {
+		adapter: adapter(),
+		alias: {
+			'~': 'src',
+		},
+	},
+	extensions: ['.svelte', '.svx', '.md'],
 };
 
 export default config;

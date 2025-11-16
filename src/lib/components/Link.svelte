066529cca035f0bfa940c5f84b631bf6eeb0c64a
Justin Bennett <git@just-be.dev>
2025-11-15 22:23:31 -0500

    Create site header

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let {
+		href,
+		target,
+		rel,
+		children,
+	}: {
+		href: string;
+		target?: string;
+		rel?: string;
+		children: Snippet;
+	} = $props();
+</script>
+
+<a
+	{href}
+	{target}
+	{rel}
+	class="transition-colors text-blue-0 hover:px-0.5 hover:-ml-0.5 hover:-mr-0.5 font-bold hover:invert hover:bg-0 focus:invert focus-visible:outline-none"
+>
+	{@render children()}
+</a>

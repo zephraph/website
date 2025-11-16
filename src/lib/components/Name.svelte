f776232bfa5fdeda8f492dc0fed33536bd06ccfd
Justin Bennett <git@just-be.dev>
2025-11-15 22:23:31 -0500

    Create site header

---
+++ src/lib/components/Name.svelte
@@ -0,0 +1,6 @@
+<div class="fixed top-0 left-1/2 -translate-x-1/2 z-20 flex items-center" style="height: 1lh;">
+	<span class="text-fg-0 font-normal">
+		<span class="font-bold">just</span><span class="font-normal text-fg-1">in</span>
+		<span class="font-bold">be</span><span class="font-normal text-fg-1">nnett</span>
+	</span>
+</div>

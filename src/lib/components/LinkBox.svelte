85edff0daddc836802d927b6175a0b240dc2df73
Justin Bennett <git@just-be.dev>
2025-11-22 11:09:08 -0500

    Improve focus states of linkbox

--- src/lib/components/LinkBox.svelte
+++ src/lib/components/LinkBox.svelte
@@ -2,8 +2,13 @@
 	let { href, title, description } = $props();
 </script>
 
-<a {href} class="block transition-colors group" box-="square" shear-="top">
-	<div class="header">
+<a
+	{href}
+	class="block transition-colors group focus-visible:outline-none focus:invert bg-0"
+	box-="square"
+	shear-="top"
+>
+	<div class="header flex">
 		<span variant-="background0 flex align-center">
 			<span class="bg-0 group-hover:text-accent invert ml-[1ch] px-[1ch] group-hover:font-bold"
 				>{title}

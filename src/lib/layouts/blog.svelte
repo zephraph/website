7464ddcc53a8d0e8926901673040cf75ba33f9dd
Justin Bennett <git@just-be.dev>
2025-11-22 14:00:09 -0500

    Cleanup the blog layout

--- src/lib/layouts/blog.svelte
+++ src/lib/layouts/blog.svelte
@@ -1,37 +1,36 @@
+<script module>
+	export { default as a } from '~/lib/components/Link.svelte';
+</script>
+
 <script>
 	import Link from '$lib/components/Link.svelte';
 
-	let { title, date, tags = [], description } = $props();
+	let { title, date, tags = [], children } = $props();
 </script>
 
 <article class="px-4 py-8">
-	<header class="mb-8 border-b border-gray-700 pb-4">
-		<h1 class="font-bold mb-2">{title}</h1>
-		{#if description}
-			<p class="text-gray-400 mb-2">{description}</p>
+	<header class="mb-[1lh]">
+		<h1 class="font-bold mb-0!">{title}</h1>
+		{#if date}
+			<time datetime={date} class="text-fg-2 inline-block"
+				>{new Date(date).toLocaleDateString('en-US', {
+					year: 'numeric',
+					month: 'long',
+					day: 'numeric',
+				})}</time
+			>
 		{/if}
-		<div class="flex items-center gap-4 text-gray-500">
-			{#if date}
-				<time datetime={date}
-					>{new Date(date).toLocaleDateString('en-US', {
-						year: 'numeric',
-						month: 'long',
-						day: 'numeric',
-					})}</time
-				>
-			{/if}
-			{#if tags.length > 0}
-				<div class="flex gap-2">
-					{#each tags as tag}
-						<span variant-="accent" class="badge">{tag}</span>
-					{/each}
-				</div>
-			{/if}
-		</div>
+		{#if tags.length > 0}
+			<div class="flex gap-2">
+				{#each tags as tag}
+					<span variant-="accent" class="badge">{tag}</span>
+				{/each}
+			</div>
+		{/if}
 	</header>
 
-	<div class="prose prose-invert max-w-none">
-		<slot />
+	<div class="max-w-none">
+		{@render children?.()}
 	</div>
 
 	<footer class="mt-12 pt-8 border-t border-gray-700">
